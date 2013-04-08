abe8785f86e9050a939e1d600543797b38af83f6
Jennifer Presto <presj521@newschool.edu>
2013-04-08 11:01:45 -0400

    Basic building instrument using ofSoundPlayer

--- 04_09_13_instrument/src/testApp.cpp
+++ 04_09_13_instrument/src/testApp.cpp
@@ -4,55 +4,41 @@
  04-09-13
  
  Assignment:
- Load an image and create a drawing
- based on it.
+ Create an instrument based on
+ data from the computer's camera.
  */
 
 #include "testApp.h"
 
 //--------------------------------------------------------------
 void testApp::setup(){
-    mtn_flora.allocate(500, 335, OF_IMAGE_COLOR);
-    sc_flora.allocate(500, 335, OF_IMAGE_COLOR);
-    mtn_flora.loadImage("mtn_flora.png");
-    sc_flora.loadImage("sc_flora.png");
+    ofBackground(255, 255, 255);
     
-    ofBackground(0, 0, 0);
-    ofEnableAlphaBlending();
+    drums.loadSound("drums.mp3");
+    cowbell.loadSound("cowbell.mp3");
     
-    pixels = mtn_flora.getPixels();
-    
-    redChange = 0;
-    
+    drums.setLoop(true);
+    cowbell.setLoop(true);
+
+    drums.play();
+    cowbell.play();
+
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-    if (mouseY>ofGetHeight()-ofGetHeight()*0.25){
-        redChange = ofMap(mouseX, 0, ofGetWidth(), -127, 127, true);
+    if(mouseY>ofGetHeight()*0.5){
+        cowbell.setVolume(1);
+    }
+    else {
+        cowbell.setVolume(0);
     }
 
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-    // draw SC picture just normally
-    ofSetColor(255, 255, 255);
-    sc_flora.draw(ofGetWidth()*0.5 - sc_flora.width*0.5, 30);
-    
-    ofLine(0, ofGetHeight()-ofGetHeight()*0.25, ofGetWidth(), ofGetHeight()-ofGetHeight()*0.25);
-    
-    // draw mtn picture on top with alpha based on mouse's Y position
-    for (int i=0; i<mtn_flora.width; i++){
-        for (int j=0; j<mtn_flora.height; j++){
-            red = pixels[(j*mtn_flora.width + i)*3];
-            green = pixels[(j*mtn_flora.width + i)*3 + 1];
-            blue = pixels[(j*mtn_flora.width + i)*3 + 2];
-            alpha = ofMap(mouseY, 30, ofGetHeight()-ofGetHeight()*0.25, 0, 255, true);
-            ofSetColor(red+redChange, green-redChange, blue, alpha);
-            ofRect(i + ofGetWidth()*0.5 - mtn_flora.width*0.5, j + 30, 1, 1);
-        }
-    }
+
 }
 
 //--------------------------------------------------------------
